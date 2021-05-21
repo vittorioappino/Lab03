7a7eb5ed7337ae6bd72048df923f0fb7fdacc33f
Vittorio <Vittorio@LAPTOP-EKSQIIDO.fritz.box>
2021-05-22 00:00:59 +0200

    3

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -5,18 +5,22 @@
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.spellchecker.model.Dictionary;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ComboBox;
 
 public class FXMLController {
 	
-	private Dictionary model;
+	private Dictionary dizionario;
+	private List<String> inputTextList;
 
     @FXML
     private ResourceBundle resources;
@@ -25,10 +29,13 @@ public class FXMLController {
     private URL location;
 
     @FXML
-    private MenuButton MenuLanguage;
+    private ComboBox<String> Menu;
 
     @FXML
     private TextArea TxtArea;
+
+    @FXML
+    private Button BtnSpellCheck;
 
     @FXML
     private TextArea TxtResult;
@@ -37,7 +44,16 @@ public class FXMLController {
     private Label TxtLabelErrors;
 
     @FXML
+    private Button BtnClearText;
+
+    @FXML
     private Label TxtFieldSpeed;
+
+
+    @FXML
+    void doActivation(ActionEvent event) {
+
+    }
 
     @FXML
     void doClearText(ActionEvent event) {
@@ -51,16 +67,23 @@ public class FXMLController {
 
     @FXML
     void initialize() {
-        assert MenuLanguage != null : "fx:id=\"MenuLanguage\" was not injected: check your FXML file 'Scene.fxml'.";
+    	assert Menu != null : "fx:id=\"Menu\" was not injected: check your FXML file 'Scene.fxml'.";
         assert TxtArea != null : "fx:id=\"TxtArea\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert BtnSpellCheck != null : "fx:id=\"BtnSpellCheck\" was not injected: check your FXML file 'Scene.fxml'.";
         assert TxtResult != null : "fx:id=\"TxtResult\" was not injected: check your FXML file 'Scene.fxml'.";
         assert TxtLabelErrors != null : "fx:id=\"TxtLabelErrors\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert BtnClearText != null : "fx:id=\"BtnClearText\" was not injected: check your FXML file 'Scene.fxml'.";
         assert TxtFieldSpeed != null : "fx:id=\"TxtFieldSpeed\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
 	public void setModel(Dictionary model) {
-		this.model=model;
+		
+		TxtArea.setText("Selezionare una lingua");
+		
+		Menu.getItems().addAll("English","Italian");
+		
+		this.dizionario=model;
 	}
 }
 
--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -1,5 +1,65 @@
 package it.polito.tdp.spellchecker.model;
 
-public class Dictionary {
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.io.BufferedReader;
 
+public class Dictionary {
+	
+	private List <String> dizionario;
+	private String language;
+
+	public boolean loadDictionary(String language) {
+		
+		if(dizionario!= null && this.language.equals(language)) {
+			return true;
+		}
+		
+		dizionario = new ArrayList<String>();
+		this.language = language;
+		
+		try {
+			FileReader fr = new FileReader("src/main/resources/" + language + ".txt");
+			BufferedReader br = new BufferedReader(fr);
+			String word;
+			
+			while((word = br.readLine()) != null) {
+				dizionario.add(word.toLowerCase());
+			}
+			
+			Collections.sort(dizionario);
+			
+			br.close();
+			System.out.println("Dizionario " + language + " loaded. Found " + dizionario.size() + " words.");
+			
+			return true;
+			
+		}catch(IOException e) {
+			System.err.println("Errore nella lettura del file");
+			return false;
+		}
+	}
+	
+	public List<RichWord> spellCheckText (List<String> inputTextList){
+		List <RichWord> parole = new ArrayList<RichWord>();
+		
+		
+		for(String s: inputTextList) {
+			RichWord richword = new RichWord(s);
+			if(dizionario.contains(s)) {
+				richword.setCorrect(true);
+				
+			}else {
+				richword.setCorrect(false);
+			}
+			
+			parole.add(richword);
+		}
+		
+		return parole;
+	}
+	
 }
